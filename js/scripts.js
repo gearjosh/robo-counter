c44c8e11149dba22333917af4a641c37920b13e4
Josh Gearheart <other.josh.gearheart@gmail.com>
2018-08-17 10:12:00 -0700

    debug empty result card

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,24 @@
-//backend logic
-var numberLister = function(input, output)
-  for (var i=1; i<=input; i++){
-    if (i = input) {
-      output.append(i);
-    } else {
-      output.append(i + ", ");
-    }
-  };
-//frontend logic
+
 $(function(){
-  var humanInput = $("#user-input").val()
-  var robotOutput = ""
   $("#input-form").submit(function(event){
+    //backend logic
+    var numberLister = function(input, output){
+      for (var i = 1; i <= input; i ++){
+        if (i.toString() === input) {
+          output += (i.toString());
+        } else {
+          output += (i.toString() + ", ");
+        }
+      };
+      robotOutput = output
+    };
+    //frontend logic
+    var humanInput = $("#user-input").val()
+    console.log(humanInput);
+    var robotOutput = ""
     event.preventDefault();
     numberLister(humanInput, robotOutput)
+    console.log(robotOutput);
     $("#result").text(robotOutput);
   });
 });
