06001ff122a5563f8cf7b75ea1d1270d929aef9e
Josh Gearheart <other.josh.gearheart@gmail.com>
2018-08-17 11:21:41 -0700

    debug array-to-string result bug

--- js/scripts.js
+++ js/scripts.js
@@ -2,22 +2,32 @@
 $(function(){
   $("#input-form").submit(function(event){
     //backend logic
-    var numberLister = function(input, output){
-      for (var i = 1; i <= input; i ++){
-        if (i.toString() === input) {
-          output += (i.toString());
-        } else {
-          output += (i.toString() + ", ");
-        }
+    var numberLister = function(number, array){
+      for (var i = 1; i <= number; i ++){
+          array.push(i);
       };
-      robotOutput = output
+      robotOutputs = array
+    };
+
+    var stringMaker = function(array, string){
+      array.forEach(function(number){
+        if (number === array.length) {
+          string += number.toString();
+        } else {
+          string += (number.toString() + ", ");
+        }
+      });
+      robotOutput = string
     };
     //frontend logic
     var humanInput = $("#user-input").val()
     console.log(humanInput);
+    var robotOutputs = []
     var robotOutput = ""
     event.preventDefault();
-    numberLister(humanInput, robotOutput)
+    numberLister(humanInput, robotOutputs)
+    console.log(robotOutputs);
+    stringMaker(robotOutputs, robotOutput);
     console.log(robotOutput);
     $("#result").text(robotOutput);
   });
